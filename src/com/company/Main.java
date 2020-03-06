13eec190d9aa0a36f64f1a7b179ce454e88cbc21
joseph were <josephboker93@gmail.com>
2020-03-06 12:36:51 +0300

    tiangudemy

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+private static Scanner scanner=new Scanner(System.in);
+    public static int[] getInteger( int number){
+      System.out.println ( "enter "+ number+" integer value.\r");
+    int[] value= new int [number];
+
+
+    for(int i=0;i<value.length;i++){
+      value [i]= scanner.nextInt();
+    }
+return value;
+    }
+
+ public static double arrayAvarage(int [] array){
+        int sum=0;
+ for (int i=0;i<array.length;i++){
+    sum+=array[i];
+
+
+ }
+ return (double) sum/(double) array.length;
+ }
+
+    public static void main(String[] args) {
+int[] numb=getInteger ( 5 );
+for(int i=0;i<numb.length;i++){
+    System.out.println ("element: " +i + " type value was " +numb[i] );
+
+
+
+
+
+}
+        System.out.println ("avarage is:" + arrayAvarage ( numb ));
+    }
+}
